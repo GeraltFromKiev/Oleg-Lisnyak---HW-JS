9241b204566ec11a1876ea538dd8944389291e75
Oleg Lisnyak <lisnyak.oleg93@gmail.com>
2019-03-15 16:50:08 +0200

    Completed all tasks.

--- main.js
+++ main.js
@@ -34,9 +34,14 @@ printStairs(3);
 // function which returns total sum of a range
 
 function sumRange(start, end) {
-  /* your code */ }
-sumRange(2, 4) // 9
-sumRange(-1, 3) // 5
+  let sum = 0;
+  for(let i = start; i <= end; i++){
+    sum+=i;
+  }
+  console.log(sum);
+}
+sumRange(2, 4); // 9
+sumRange(-1, 3); // 5
 
 //Write a function which returns the smallest of three numbers.
 function min(a, b, c) {
@@ -50,26 +55,27 @@ min(10, 5, 11); // 5
 min(3, 8, 4); // 3
 
 // function which prints the pyramid picture of size n
-function printPyramid(n) {
-//Not done yet...
-//   function printStairs(n) {
-//     var space, hash, stair, space2;
-//     for (var i = 0; i < n; i++){
-//         space = n - 1 - i; // считает кол-во пробело
-//         space2 = space + 1;
-//         hash = i + 1; // считаем кол-во звёзд
-//         stair = ' '.repeat(space2) + '#'.repeat(hash) + ' '.repeat(space); // добавляем/заменяем повторения согласно
-//         //кол-ву которые посчитали на каждом шагу hash & space.
-//         console.log(stair);
-//     }
-// }
-// printStairs(3);
+function printPyramid(n){
+	for(let i = 0; i < n; i++) {
+	  if(i % 2 === 0){
+		  let output = '';
+		    for(let j = 0; j < n-i; j++) {
+		        output += ' ';
+		    }
+		    for(let k = 0 ; k <= i; k++) {
+		       output += '# ';
+		    }
+		    console.log(output);
+		}
+	}
 }
-printStairs(3)
+printPyramid(14);
 
 // function which transforms first and last letter to uppercase(use built in string’ s method).
 function firstAndLastToUpper(str) {
-  /* your code */ }
+  if (!str) return str;
+  return str[0].toUpperCase() + str[2].toUpperCase() + str.slice(1);
+}
 firstAndLastToUpper('abc') // 'AbC'
 
 // function which returns true
@@ -77,7 +83,9 @@ firstAndLastToUpper('abc') // 'AbC'
 // function must be
 // case -insensitive.
 function cursorCheck(str) {
-  /* your code */ }
+  let lowerStr = str.toLowerCase();
+  return !!(~lowerStr.indexOf('ostap') || ~lowerStr.indexOf('ironman') || ~lowerStr.indexOf('cursor'));
+}
 cursorCheck('Hello I am OstaP') // true
 cursorCheck('Superman is here') // false
 
@@ -85,21 +93,32 @@ cursorCheck('Superman is here') // false
 // The function supports only english alphabet.
 
 function toUppercase(str) {
-  /* your code */ }
-toUppercase('abc') // 'ABC'
+  return (str.toUpperCase());
+}
+toUppercase('abc'); // 'ABC'
 
 // function which removes duplication of letters in string.The
 // function must be
 // case -insensitive.
 
-function removeDuplicationLetters(str) {
-  /* your code */ }
-removeDuplicationLetters('Hello I am Iron Man') // 'Helo I am rn '
+function removeDuplicationLetters(str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 7) + '...' : str;
+}
+removeDuplicationLetters('Hello I am Iron Man', 18); // 'Helo I am rn '
 
 // function that when given a number n returns the n - th number in the Fibonacci Sequence.The sequence starts from 1 and looks like this: 1, 1, 2, 3, 5, 8, 13, 21…
 
 function fibonacci(n) {
-  /* your code */ }
-fibonacci(3) // 2
-fibonacci(5) // 5
-fibonacci(7) // 13
+  let a = 1,
+    b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+fibonacci(3); // 2
+fibonacci(5); // 5
+fibonacci(7); // 13
